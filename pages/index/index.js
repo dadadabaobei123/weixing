b020bbeb02ac7ac92b6d0f628b7df1acafdf6b55
dadadabaobei123 <1550484041@qq.com>
2020-03-25 21:35:55 +0800

    www

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,12 +7,14 @@ Page({
     // 轮播数据
     swiperlist:[],
     // 导航数据
-    daohanglist:[]
+    daohanglist:[],
+    loucenglist:[]
   },
   // 页面开始加载就会触发
   onLoad:function(options){
     this.getswiperlist()
     this.gitdaohanglist()
+    this.gitloucenglist()
   },
   getswiperlist(){
 // 发送异步请求获取轮播数据
@@ -22,10 +24,19 @@ request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'})
   })
 })
   },
+  // 发送异步请求获取导航数据
   gitdaohanglist(){
     request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'}).then(result=>{
+      this.setData({
+        daohanglist:result.data.message
+      })
+})
+  },
+  // 发送异步请求获取楼层数据
+  gitloucenglist(){
+    request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'}).then(result=>{
   this.setData({
-    daohanglist:result.data.message
+    loucenglist:result.data.message
   })
 })
   }
